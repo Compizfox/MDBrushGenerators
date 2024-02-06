Compizfox/MDBrushGenerators

commit 46d853c5de3e1c6cae6ef49ddbe71ce752bdeb94

Make PoissonDiskGenerator blazingly fast by rewriting it in Rust 🦀 ~300x speedup

BrushGenerator.py
@@ -48,7 +48,7 @@ class BrushGenerator(ABC):
 		:param n_beads:        Chain length.
 		:param bottom_padding: Distance between the bottom edge of the box and the grafting layer. Must be positive.
 		"""
-		self.box_size = list(box_size)
+		self.box_size = box_size
 		self.rng_seed = rng_seed
 		self.bead_size = bead_size
 		self.n_beads = n_beads
@@ -90,8 +90,8 @@ class BrushGenerator(ABC):
 		:return Number of grafting points generated.
 		"""
 		# Generate grafting point coordinates
-		pdg = PoissonDiskGenerator(self.rng_seed)
-		self.coordinates = pdg.generate(n_chains, self.bead_size, self.box_size[0:2], max_overlap_iter)
+		points = PoissonDiskGenerator.gen(n_chains, self.bead_size, self.box_size[0:2], self.rng_seed, max_overlap_iter)
+		self.coordinates = np.asarray(points)
 
 		return len(self.coordinates)
 

.github/workflows/CI.yml
@@ -1,0 +1,120 @@
+# This file is autogenerated by maturin v1.2.3
+# To update, run
+#
+#    maturin generate-ci github
+#
+name: CI
+
+on:
+  push:
+    branches:
+      - main
+      - master
+    tags:
+      - '*'
+  pull_request:
+  workflow_dispatch:
+
+permissions:
+  contents: read
+
+jobs:
+  linux:
+    runs-on: ubuntu-latest
+    strategy:
+      matrix:
+        target: [x86_64, x86, aarch64, armv7, s390x, ppc64le]
+    steps:
+      - uses: actions/checkout@v3
+      - uses: actions/setup-python@v4
+        with:
+          python-version: '3.10'
+      - name: Build wheels
+        uses: PyO3/maturin-action@v1
+        with:
+          target: ${{ matrix.target }}
+          args: --release --out dist --find-interpreter
+          sccache: 'true'
+          manylinux: auto
+      - name: Upload wheels
+        uses: actions/upload-artifact@v3
+        with:
+          name: wheels
+          path: dist
+
+  windows:
+    runs-on: windows-latest
+    strategy:
+      matrix:
+        target: [x64, x86]
+    steps:
+      - uses: actions/checkout@v3
+      - uses: actions/setup-python@v4
+        with:
+          python-version: '3.10'
+          architecture: ${{ matrix.target }}
+      - name: Build wheels
+        uses: PyO3/maturin-action@v1
+        with:
+          target: ${{ matrix.target }}
+          args: --release --out dist --find-interpreter
+          sccache: 'true'
+      - name: Upload wheels
+        uses: actions/upload-artifact@v3
+        with:
+          name: wheels
+          path: dist
+
+  macos:
+    runs-on: macos-latest
+    strategy:
+      matrix:
+        target: [x86_64, aarch64]
+    steps:
+      - uses: actions/checkout@v3
+      - uses: actions/setup-python@v4
+        with:
+          python-version: '3.10'
+      - name: Build wheels
+        uses: PyO3/maturin-action@v1
+        with:
+          target: ${{ matrix.target }}
+          args: --release --out dist --find-interpreter
+          sccache: 'true'
+      - name: Upload wheels
+        uses: actions/upload-artifact@v3
+        with:
+          name: wheels
+          path: dist
+
+  sdist:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - name: Build sdist
+        uses: PyO3/maturin-action@v1
+        with:
+          command: sdist
+          args: --out dist
+      - name: Upload sdist
+        uses: actions/upload-artifact@v3
+        with:
+          name: wheels
+          path: dist
+
+  release:
+    name: Release
+    runs-on: ubuntu-latest
+    if: "startsWith(github.ref, 'refs/tags/')"
+    needs: [linux, windows, macos, sdist]
+    steps:
+      - uses: actions/download-artifact@v3
+        with:
+          name: wheels
+      - name: Publish to PyPI
+        uses: PyO3/maturin-action@v1
+        env:
+          MATURIN_PYPI_TOKEN: ${{ secrets.PYPI_API_TOKEN }}
+        with:
+          command: upload
+          args: --non-interactive --skip-existing *

PoissonDiskGenerator.py
@@ -1,102 +1,0 @@
-"""
-Exports the PoissonDiskGenerator class.
-"""
-
-from itertools import product
-from typing import Optional, Tuple
-
-import numpy as np
-
-
-class PoissonDiskGenerator:
-	"""
-	Generate a Poisson-disk point set using cell list accelerated dart throwing.
-	"""
-
-	def __init__(self, seed: Optional[int]):
-		"""
-		:param seed: Seed used to initialize the PRNG. May be None, in which case a random seed will be used.
-		"""
-		self.rng = np.random.RandomState(seed=seed)
-
-		# Create a list of 20 neighbors (5x5 excluding corners and center)
-		self.neighbor_matrix = []
-		for i in [-2, -1, 0, 1, 2]:
-			for j in [-2, -1, 0, 1, 2]:
-				if not ((abs(i) == 2 and abs(j) == 2) or (i == 0 and j == 0)):
-					self.neighbor_matrix.append((i, j))
-
-	def generate(self, n: int, bead_size: float, size: Tuple[float, float], max_iter: int = 1000) -> np.ndarray:
-		"""
-		:param n:         Number of points.
-		:param bead_size: Minimum distance between points.
-		:param size:      2-Tuple corresponding to the (2d) domain size.
-		:param max_iter:  Iteration limit.
-		:return Ndarray of shape (n, 2) with all point coordinates.
-		"""
-
-		bead_size_squared = bead_size**2
-
-		def check_overlap(cell: np.ndarray, point: np.ndarray) -> bool:
-			"""
-			Check for overlap in the 8 cells neighboring the current cell.
-			:param cell:  Ndarray of shape (2) containing cell indices.
-			:param point: Ndarray of shape (2) containing the point coordinates.
-			:return Return True if point overlaps, False if not.
-			"""
-			for i, j in self.neighbor_matrix:
-				x = cell[0] + i
-				y = cell[1] + j
-				if 0 <= x <= (x_max - 1) and 0 <= y <= (y_max - 1):
-					coord = grid_lookup[x][y]
-					if coord is not None:
-						if sum((point - coord)**2) < bead_size_squared:
-							return True
-
-			return False
-
-		def check_oob(point: np.ndarray) -> bool:
-			"""
-			Check if the point is out-of-bounds, which can happen because the domain the algorithm runs on is
-			slightly larger than the requested domain because of rasterising errors.
-			:param  point: Ndarray of shape (2) containing the point coordinates.
-			:return Return True if point is out-of-bounds, False if not.
-			"""
-			return (point > np.array(size)).all()
-
-		# Create a grid of square background cells. The cells should be as large as possible but while still being fully
-		# covered by the size of a point within it.
-		cell_size = bead_size/np.sqrt(2)
-
-		# Round desired domain size up to nearest multiple of cell size
-		x_max = int(np.ceil(size[0]/cell_size))
-		y_max = int(np.ceil(size[1]/cell_size))
-		# List of indices of cells. Coordinate of bottom left corner = (i*cell_size, j*cell_size)
-		active_cells = np.array(list(product(range(x_max), range(y_max))))
-
-		# x*y empty list
-		grid_lookup = [[None]*y_max for i in range(x_max)]
-
-		coordinates = []
-		for count in range(0, max_iter):
-			# Choose random active cell
-			if len(active_cells) == 0:
-				break
-			cell_id = self.rng.randint(low=0, high=len(active_cells))
-
-			# Throw a dart
-			rnd = self.rng.random_sample(2)
-			x = active_cells[cell_id, 0] + rnd[0]
-			y = active_cells[cell_id, 1] + rnd[1]
-			point = np.array([x, y])*cell_size
-
-			# Check if point overlaps in neighbouring cells
-			if not check_overlap(active_cells[cell_id, :], point) and not check_oob(point):
-				coordinates.append(point)
-				grid_lookup[active_cells[cell_id, 0]][active_cells[cell_id, 1]] = point
-				active_cells = np.delete(active_cells, cell_id, axis=0)
-
-			if len(coordinates) >= n:
-				break
-
-		return np.array(coordinates)

PoissonDiskGenerator/.gitignore
@@ -1,0 +1,72 @@
+/target
+
+# Byte-compiled / optimized / DLL files
+__pycache__/
+.pytest_cache/
+*.py[cod]
+
+# C extensions
+*.so
+
+# Distribution / packaging
+.Python
+.venv/
+env/
+bin/
+build/
+develop-eggs/
+dist/
+eggs/
+lib/
+lib64/
+parts/
+sdist/
+var/
+include/
+man/
+venv/
+*.egg-info/
+.installed.cfg
+*.egg
+
+# Installer logs
+pip-log.txt
+pip-delete-this-directory.txt
+pip-selfcheck.json
+
+# Unit test / coverage reports
+htmlcov/
+.tox/
+.coverage
+.cache
+nosetests.xml
+coverage.xml
+
+# Translations
+*.mo
+
+# Mr Developer
+.mr.developer.cfg
+.project
+.pydevproject
+
+# Rope
+.ropeproject
+
+# Django stuff:
+*.log
+*.pot
+
+.DS_Store
+
+# Sphinx documentation
+docs/_build/
+
+# PyCharm
+.idea/
+
+# VSCode
+.vscode/
+
+# Pyenv
+.python-version

PoissonDiskGenerator/Cargo.toml
@@ -1,0 +1,14 @@
+[package]
+name = "PoissonDiskGenerator"
+version = "0.1.0"
+edition = "2021"
+
+# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+[lib]
+name = "PoissonDiskGenerator"
+crate-type = ["cdylib"]
+
+[dependencies]
+itertools = "0.11.0"
+pyo3 = "0.19.0"
+rand = { version = "0.8.5", features = ["small_rng"] }

PoissonDiskGenerator/src/lib.rs
@@ -1,0 +1,108 @@
+use std::f64::consts::SQRT_2;
+
+use itertools::Itertools;
+use pyo3::prelude::*;
+use rand::prelude::*;
+
+
+/// Generate a 2D Poisson-disk point set using cell list accelerated dart throwing.
+/// * `n` - Number of points
+/// * `bead_size` - Minimum distance between points
+/// * `size` - 2-tuple corresponding to the domain size
+/// * `seed` - Optional seed number for PRNG
+/// * `max_iter` - Optional iteration limit
+/// Returns a list of tuples corresponding to the point coordinates.
+#[pyfunction]
+fn gen(n: usize, bead_size: f64, size: (f64, f64), seed: Option<u64>, max_iter: Option<i32>) -> PyResult<Vec<(f64, f64)>> {
+    // Create a grid of square background cells. The cells should be as large as possible but while still being fully
+    // covered by the size of a point within it.
+    let cell_size = bead_size / SQRT_2;
+
+    // Round desired domain size up to nearest multiple of cell size
+    let max = (
+        (size.0 / cell_size).ceil() as usize,
+        (size.1 / cell_size).ceil() as usize
+    );
+
+    // Flat list of indices of cells. Coordinate of bottom left corner = (i*cell_size, j*cell_size)
+    let mut active_cells: Vec<(usize, usize)> = (0..max.0).cartesian_product(0..max.1).collect();
+    // 2D cell list
+    let mut cell_list: Vec<Vec<Option<(f64, f64)>>> = vec![vec![None; max.1]; max.0];
+    // List of point coordinates
+    let mut coordinates: Vec<(f64, f64)> = Vec::new();
+
+    // Initialize PRNG
+    let mut small_rng = match seed {
+        Some(x) => SmallRng::seed_from_u64(x),
+        None => SmallRng::from_entropy(),
+    };
+
+    /// Check for overlap in the cells neighboring the current cell.
+    /// * `cell_list` - The cell list
+    /// * `central_cell` - Indices of the central cell
+    /// * `a` - Coordinates of the point
+    /// Returns true if point overlaps, false if not.
+    let check_overlap = |cell_list: &Vec<Vec<Option<(f64, f64)>>>, central_cell: (usize, usize), a: (f64, f64)| -> bool {
+        // Check 20 neighbors (5x5 excluding corners and center)
+        for (i, j) in [(-2, -1), (-2, 0), (-2, 1), (-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2), (0, -2), (0, -1), (0, 1), (0, 2), (1, -2), (1, -1), (1, 0), (1, 1), (1, 2), (2, -1), (2, 0), (2, 1)] {
+            let neigh_cell = (
+                central_cell.0 as i32 + i,
+                central_cell.1 as i32 + j,
+            );
+            if (neigh_cell.0 > 0) & ((neigh_cell.0 as usize) < max.0 - 1) &&
+               (neigh_cell.1 > 0) & ((neigh_cell.1 as usize) < max.1 - 1) {
+                if cell_list[neigh_cell.0 as usize][neigh_cell.1 as usize]
+                    .is_some_and(|b| (a.0 - b.0).powi(2) + (a.1 - b.1).powi(2) < bead_size.powi(2)) {
+                    return true
+                }
+            }
+        }
+        return false
+    };
+
+    /// Check if the point is out-of-bounds, which can happen because the domain the algorithm runs on is slightly
+    /// larger than the requested domain because of rasterising errors.
+    /// * `point` - Coordinates of the point
+    /// Returs true if point is out-of-bounds, false if not.
+    let check_oob = |point: (f64, f64)| -> bool {
+        point.0 > size.0 || point.1 > size.1
+    };
+
+    let max_iter = max_iter.unwrap_or(1000);
+    for _ in 0..max_iter {
+        if active_cells.len() == 0 {
+            println!("Maximal point set reached.");
+            break
+        }
+
+        // Choose random active cell
+        let cell_id = small_rng.gen_range(0..active_cells.len());
+        let cell = active_cells[cell_id];
+
+        // Throw a dart
+        let point = (
+            (cell.0 as f64 + random::<f64>()) * cell_size,
+            (cell.1 as f64 + random::<f64>()) * cell_size,
+        );
+
+        // Check whether point overlaps in neighbouring cells or is OOB
+        if !check_overlap(&cell_list, cell, point) && !check_oob(point) {
+            coordinates.push(point);
+            cell_list[cell.0][cell.1] = Some(point);
+            active_cells.swap_remove(cell_id);
+        }
+
+        if coordinates.len() >= n {
+            break;
+        }
+    }
+
+    println!("Generated {} points", coordinates.len());
+    Ok(coordinates)
+}
+
+#[pymodule]
+fn PoissonDiskGenerator(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(gen, m)?)?;
+    Ok(())
+}

pyproject.toml
@@ -1,0 +1,30 @@
+[build-system]
+requires = ["maturin>=1.2,<2.0", "poetry-core"]
+build-backend = "maturin"
+
+[project]
+name = "PoissonDiskGenerator"
+requires-python = ">=3.7"
+classifiers = [
+    "Programming Language :: Rust",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+]
+
+
+[tool.maturin]
+features = ["pyo3/extension-module"]
+
+[tool.poetry]
+name = "PoissonDiskGenerator"
+version = "0.1.0"
+description = ""
+authors = ["Lars Veldscholte <lars@tuxplace.nl>"]
+readme = "README.md"
+
+[tool.poetry.dependencies]
+python = "^3.11"
+numpy = "^1.25.2"
+matplotlib = "^3.8.0"
+maturin = "^1.2.3"
+pandas = "^2.1.0"

commit 15b8b41181a842190249b2223626741936f086b3

Fix regression caused by 46d853c: make self.box_size a list, and convert it back to tuple when calling PoissonDiskGenerator.gen()

BrushGenerator.py
@@ -48,7 +48,7 @@ class BrushGenerator(ABC):
 		:param n_beads:        Chain length.
 		:param bottom_padding: Distance between the bottom edge of the box and the grafting layer. Must be positive.
 		"""
-		self.box_size = box_size
+		self.box_size = list(box_size)
 		self.rng_seed = rng_seed
 		self.bead_size = bead_size
 		self.n_beads = n_beads
@@ -90,7 +90,7 @@ class BrushGenerator(ABC):
 		:return Number of grafting points generated.
 		"""
 		# Generate grafting point coordinates
-		points = PoissonDiskGenerator.gen(n_chains, self.bead_size, self.box_size[0:2], self.rng_seed, max_overlap_iter)
+		points = PoissonDiskGenerator.gen(n_chains, self.bead_size, tuple(self.box_size[0:2]), self.rng_seed, max_overlap_iter)
 		self.coordinates = np.asarray(points)
 
 		return len(self.coordinates)